538ffe80da450cce09fe0d760d24edddf51a8f88
Kuben <jlawniczak@edu.cdv.pl>
2026-09-21 00:51:37 +0200

    Add speedinsights, analytics and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Portfolio from "@/components/sections/Portfolio";
 import Contact from "@/components/sections/Contact";
 import FAQ from "@/components/sections/FAQ";
 import About from "@/components/sections/About";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
     return (
@@ -16,6 +18,8 @@ export default function Home() {
             <Process/>
             <FAQ/>
             <Contact />
+            <Analytics />
+            <SpeedInsights/>
         </main>
     );
 }
