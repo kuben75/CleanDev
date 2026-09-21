15dfe48e53a5c86eb629f787b9370b80bac2ceea
Kuben <jlawniczak@edu.cdv.pl>
2026-09-21 23:54:27 +0200

    small fixes pt.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import ScrollToTop from "@/components/ui/ScrollToTop";
 import CookieBanner from "@/components/ui/CookieBanner";
 import {siteConfig} from "@/config/site";
 import React from "react";
-// import {Analytics} from "@vercel/analytics/vue";
-// import {SpeedInsights} from "@vercel/speed-insights/vue";
+import {Analytics} from "@vercel/analytics/vue";
+import {SpeedInsights} from "@vercel/speed-insights/vue";
 
 const inter = Inter({
     subsets: ["latin", "latin-ext"],
@@ -60,8 +60,8 @@ export default function RootLayout({
             <Footer />
             <ScrollToTop />
             <CookieBanner/>
-            {/*<Analytics />*/}
-            {/*<SpeedInsights/>*/}
+            <Analytics />
+            <SpeedInsights/>
         </ThemeProvider>
         </body>
         </html>
